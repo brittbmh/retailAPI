db4a45bae86463de304806455b13f74cde471134
Britt Carter <brittbmh@gmail.com>
2019-07-20 13:14:34 -0500

    add sample products for database

---
+++ products.js
@@ -0,0 +1,50 @@
+{
+    "id": 13860428,
+        "value": 14.99,
+            "currency_code": "USD"
+}
+{
+    "id": 12149250,
+        "value": 9.69,
+            "currency_code": "USD"
+}
+{
+    "id": 12110209,
+        "value": 64.99,
+            "currency_code": "USD"
+}
+{
+    "id": 52349598,
+        "value": 313.49,
+            "currency_code": "USD"
+}
+{
+    "id": 52343339,
+        "value": 79.99,
+            "currency_code": "USD"
+}
+{
+    "id": 53782617,
+        "value": 39.99,
+            "currency_code": "USD"
+}
+{
+    "id": 53192988,
+        "value": 29.99,
+            "currency_code": "USD"
+},
+{
+    "id": 53741474,
+        "value": 99.99,
+            "currency_code": "USD"
+}
+{
+    "id": 53306858,
+        "value": 22.99,
+            "currency_code": "USD"
+}
+{
+    "id": 14168377,
+        "value": 49.99,
+            "currency_code": "USD"
+}
