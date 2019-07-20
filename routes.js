b1267a43a6379af764a74deb64d73a8e3b24cb80
Britt Carter <brittbmh@gmail.com>
2019-07-20 10:40:44 -0500

    completed base get request

--- routes.js
+++ routes.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+const BASE_URL = 'https://redsky.target.com/v2/pdp/tcin/'
+
+const data = [{
+    "id": 13860428,
+    "name": "The Big Lebowski (Blu-ray) (Widescreen)",
+    "current_price": {
+        "value": 13.49,
+        "currency_code": "USD"
+    }
+}];
+
 
 router.get('/:id', (req, res) => {
-    console.log('In GET by id');
-    res.json(["Roger", "Don", "Peggy", "Joan", "Pete"]);
+    const id = req.params.id;
+    console.log('In GET by id', id);
+    let result = {current_price: {currency_code: "USD"}};
+    let URL = `${BASE_URL}${id}?excludes=taxonomy,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics`;
+    axios.get(URL).then((response) => {
+        result.id = response.data.product.item.tcin;
+        result.name = response.data.product.item.product_description.title;
+        result.current_price.value = response.data.product.price.offerPrice.price;
+        console.log(result);
+        res.send(result);
+    }).catch(error => {
+        console.log('Error in Get by id', error);
+        res.sendStatus(500);
+    })
 })
 
 module.exports = router;
